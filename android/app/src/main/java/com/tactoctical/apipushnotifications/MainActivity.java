0d534cf30dcf8f402d5bd131dea4a98df2fb491e
viktorholk <viktorholk@gmail.com>
2020-10-01 08:38:30 +0200

    android init

---
+++ android/app/src/main/java/com/tactoctical/apipushnotifications/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tactoctical.apipushnotifications;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
